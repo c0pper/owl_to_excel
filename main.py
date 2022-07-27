b59209c4a374981d975ff3f1d87556dc5fa6ae8b
93simon <93simonster@gmail.com>
2022-07-27 12:11:37 +0200

    added properties sheet

--- main.py
+++ main.py
@@ -8,13 +8,15 @@ def render_using_name(entity):
 
 set_render_func(render_using_name)
 
-onto = get_ontology("Silvanus ontology(con seeAlso).owl").load()
+onto = get_ontology("onto.owl").load()
 
 wb = Workbook()
-ws = wb.active
+classes_ws = wb.create_sheet("classes")
+properties_ws = wb.create_sheet("properties")
 
 classes = list(onto.classes())
-# with open("classes.csv", "w", encoding="UTF8") as f:
+properties = list(onto.object_properties())
+
 line = 2
 for c in classes:
     if c.comment:
@@ -28,23 +30,54 @@ for c in classes:
     if c.seeAlso:
         for s in c.seeAlso:
             sources.append(s)
-    # line = f"{c}§{desc}§{c.is_a}§{list(c.subclasses())}§{sources}\n"
-    # f.write(line)
     parents = map(str, list(c.is_a))
     subclasses = map(str, list(c.subclasses()))
-    print(list(subclasses))
 
-    ws[f"A{line}"] = "Class name"
-    ws[f"A{line+1}"] = "Class description"
-    ws[f"A{line+2}"] = "Class parents"
-    ws[f"A{line+3}"] = "Subclasses"
-    ws[f"A{line+4}"] = "Sources"
+    classes_ws[f"A{line}"] = "Class name"
+    classes_ws[f"A{line + 1}"] = "Class description"
+    classes_ws[f"A{line + 2}"] = "Class parents"
+    classes_ws[f"A{line + 3}"] = "Subclasses"
+    classes_ws[f"A{line + 4}"] = "Sources"
 
-    ws[f"B{line}"] = str(c)
-    ws[f"B{line+1}"] = desc
-    ws[f"B{line+2}"] = ", ".join(parents)
-    ws[f"B{line+3}"] = ", ".join(subclasses)
-    ws[f"B{line+4}"] = ", ".join(sources)
+    classes_ws[f"B{line}"] = str(c)
+    classes_ws[f"B{line + 1}"] = desc
+    classes_ws[f"B{line + 2}"] = ", ".join(parents)
+    classes_ws[f"B{line + 3}"] = ", ".join(subclasses)
+    classes_ws[f"B{line + 4}"] = ", ".join(sources)
     line += 6
 
+line = 2
+for p in properties:
+    name = p.name
+    desc = p.comment[0]
+    domain = list(map(render_using_name, list(p.domain)))
+    range = list(map(render_using_name, list(p.range)))
+
+    properties_ws[f"A{line}"] = "Property name"
+    properties_ws[f"A{line + 1}"] = "Property description"
+    properties_ws[f"A{line + 2}"] = "Property domain"
+    properties_ws[f"A{line + 3}"] = "Property range"
+
+    properties_ws[f"B{line}"] = name
+    properties_ws[f"B{line + 1}"] = desc
+    properties_ws[f"B{line + 2}"] = ", ".join(domain)
+    properties_ws[f"B{line + 3}"] = ", ".join(range)
+    line += 5
+
 wb.save("onto.xlsx")
+
+
+# adding comments to properties automatically
+# for p in properties:
+#     print(p.name)
+#     if p.comment:
+#         comment = f"original: {p.comment[0]}"
+#     else:
+#         domain = p.domain
+#         if len(domain) == 1:
+#             comment = f"Links {p.domain[0]} to {p.range[0]}".replace("_", " ")
+#         else:
+#             comment = f"Links some entites to {p.range[0]}".replace("_", " ")
+#         p.comment = comment
+#     print(comment)
+# onto.save(file = "onto.owl", format = "rdfxml")
