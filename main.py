ae99ccbd8307c6f69201e0fc747a20009a9eb1ad
93simon <93simonster@gmail.com>
2022-08-03 09:16:55 +0200

    added print subclasses and properties

--- main.py
+++ main.py
@@ -10,61 +10,62 @@ set_render_func(render_using_name)
 
 onto = get_ontology("onto.owl").load()
 
-wb = Workbook()
-classes_ws = wb.create_sheet("classes")
-properties_ws = wb.create_sheet("properties")
-
 classes = list(onto.classes())
 properties = list(onto.object_properties())
 
-line = 2
-for c in classes:
-    if c.comment:
-        desc = c.comment[0].replace("\n", " ")
-    else:
-        desc = ""
-    sources = list()
-    if c.source:
-        for s in c.source:
-            sources.append(s)
-    if c.seeAlso:
-        for s in c.seeAlso:
-            sources.append(s)
-    parents = map(str, list(c.is_a))
-    subclasses = map(str, list(c.subclasses()))
+if __name__ == "__main__":
+    wb = Workbook()
+    classes_ws = wb.create_sheet("classes")
+    properties_ws = wb.create_sheet("properties")
 
-    classes_ws[f"A{line}"] = "Class name"
-    classes_ws[f"A{line + 1}"] = "Class description"
-    classes_ws[f"A{line + 2}"] = "Class parents"
-    classes_ws[f"A{line + 3}"] = "Subclasses"
-    classes_ws[f"A{line + 4}"] = "Sources"
+    line = 2
+    for c in classes:
+        if c.comment:
+            desc = c.comment[0].replace("\n", " ")
+        else:
+            desc = ""
+        sources = list()
+        if c.source:
+            for s in c.source:
+                sources.append(s)
+        if c.seeAlso:
+            for s in c.seeAlso:
+                sources.append(s)
+        parents = map(str, list(c.is_a))
+        subclasses = map(str, list(c.subclasses()))
 
-    classes_ws[f"B{line}"] = str(c)
-    classes_ws[f"B{line + 1}"] = desc
-    classes_ws[f"B{line + 2}"] = ", ".join(parents)
-    classes_ws[f"B{line + 3}"] = ", ".join(subclasses)
-    classes_ws[f"B{line + 4}"] = ", ".join(sources)
-    line += 6
+        classes_ws[f"A{line}"] = "Class name"
+        classes_ws[f"A{line + 1}"] = "Class description"
+        classes_ws[f"A{line + 2}"] = "Class parents"
+        classes_ws[f"A{line + 3}"] = "Subclasses"
+        classes_ws[f"A{line + 4}"] = "Sources"
 
-line = 2
-for p in properties:
-    name = p.name
-    desc = p.comment[0]
-    domain = list(map(render_using_name, list(p.domain)))
-    range = list(map(render_using_name, list(p.range)))
+        classes_ws[f"B{line}"] = str(c)
+        classes_ws[f"B{line + 1}"] = desc
+        classes_ws[f"B{line + 2}"] = ", ".join(parents)
+        classes_ws[f"B{line + 3}"] = ", ".join(subclasses)
+        classes_ws[f"B{line + 4}"] = ", ".join(sources)
+        line += 6
 
-    properties_ws[f"A{line}"] = "Property name"
-    properties_ws[f"A{line + 1}"] = "Property description"
-    properties_ws[f"A{line + 2}"] = "Property domain"
-    properties_ws[f"A{line + 3}"] = "Property range"
+    line = 2
+    for p in properties:
+        name = p.name
+        desc = p.comment[0]
+        domain = list(map(render_using_name, list(p.domain)))
+        range = list(map(render_using_name, list(p.range)))
 
-    properties_ws[f"B{line}"] = name
-    properties_ws[f"B{line + 1}"] = desc
-    properties_ws[f"B{line + 2}"] = ", ".join(domain)
-    properties_ws[f"B{line + 3}"] = ", ".join(range)
-    line += 5
+        properties_ws[f"A{line}"] = "Property name"
+        properties_ws[f"A{line + 1}"] = "Property description"
+        properties_ws[f"A{line + 2}"] = "Property domain"
+        properties_ws[f"A{line + 3}"] = "Property range"
 
-wb.save("onto.xlsx")
+        properties_ws[f"B{line}"] = name
+        properties_ws[f"B{line + 1}"] = desc
+        properties_ws[f"B{line + 2}"] = ", ".join(domain)
+        properties_ws[f"B{line + 3}"] = ", ".join(range)
+        line += 5
+
+    wb.save("onto.xlsx")
 
 
 # adding comments to properties automatically
---
+++ print_subclasses.py
@@ -0,0 +1,62 @@
+from main import onto, render_using_name, classes, properties
+
+fire_subclasses = onto.Fire.subclasses()
+spatial_subclasses = onto.Spatial_entity.subclasses()
+
+def subclasses_list_to_str(onto_class):
+    subclasses = list(onto_class.subclasses())
+    if len(subclasses) > 0:
+        if len(subclasses) == 2:
+            subclass1 = subclasses[0].name.replace("_", " ")
+            subclass2 = subclasses[1].name.replace("_", " ")
+            subclasses_str = f"{subclass1}\" and \"{subclass2}"
+        else:
+            subclasses_str = "\", \"".join(list(map(render_using_name, subclasses))).replace("_", " ")
+        if onto_class.comment:
+            return_str = f"\"{onto_class.name.replace('_', ' ')}\" class contains \"" + subclasses_str + f"\". {onto_class.comment[0]}"
+        else:
+            return_str = f"\"{onto_class.name.replace('_', ' ')}\" class contains \"" + subclasses_str + f"\"."
+        if return_str[-1] != ".":
+            return_str += "."
+        print(return_str)
+
+
+def get_prop_desc_for_class(class_):
+    for p in properties:
+        name = p.name.replace("_", " ")
+        domain = list(map(render_using_name, p.domain))
+        range = list(map(render_using_name, p.range))
+        if class_.name in domain or class_.name in range:
+            domani_str = "\", \"".join(domain).replace("_", " ")
+            range_str = "\", \"".join(range).replace("_", " ")
+            # if p.comment:
+            #     ret_str = f'Property "{name}" links class "{domani_str}" to class "{range_str}". {p.comment[0]}'
+            # else:
+            #     ret_str = f'Property "{name}" links class "{domani_str}" to class "{range_str}"'
+            ret_str = f'Property "{name}" links class "{domani_str}" to class "{range_str}"'
+            if ret_str[-1] != ".":
+                ret_str += "."
+            print(ret_str)
+
+def print_subclasses_and_props(class_):
+    classes_ = list(class_.subclasses())
+    if len(classes_) > 0:
+        if len(classes_) == 2:
+            subclass1 = classes_[0].name.replace("_", " ")
+            subclass2 = classes_[1].name.replace("_", " ")
+            subclasses_str = f"{subclass1}\" and \"{subclass2}"
+        else:
+            subclasses_str = "\", \"".join(list(map(render_using_name, classes_))).replace("_", " ")
+    print(f'The main classes in this area are "{subclasses_str}".')
+    for c in classes_:
+        subclasses_list_to_str(c)
+    print("Regarding properties, those that affect this macro-area are the following:")
+    get_prop_desc_for_class(class_)
+
+
+
+classes_ = [onto.Biodiversity_index, onto.Vegetated_area, onto.Vegetation_stat, onto.Living_being_stat, onto.Sensor, onto.Climate_parameter, onto.Response_resource, onto.Monitored_area, onto.Vulnerable_object]
+for c in classes_:
+    print(c.name)
+    print_subclasses_and_props(c)
+    print("\n\n")
